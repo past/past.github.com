f4cc88f517272b0c724b827514e01ad416a55734
Panagiotis Astithas <pastith@gmail.com>
2013-11-03 23:23:15 +0200

    More updates

---
+++ talks/qconsf2013/js/breakpoints.js
@@ -0,0 +1,39 @@
+gState = {
+  iteration: 0,
+  time: 0,
+  timeString: null
+}
+
+function loop(num) {
+  for(var i = 0; i < num; i++) {
+    console.log("Iteration: " + i);
+
+    gState.iteration = i;
+    gState.time = Date.now();
+    gState.timeString = new Date(gState.time).toUTCString();
+
+    try {
+      thrower();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  buggy();
+  console.log("Reached " + i);
+}
+
+function thrower() {
+  throw new Error("Oops!");
+}
+
+function buggy() {
+  boom();
+}
+
+window.addEventListener("load", function(event) {
+  var breakpointBtn = document.querySelector("#breakpoint-btn");
+  breakpointBtn.addEventListener("click", function onClick(event) {
+    loop(100);
+  }, false);
+}, false);
---
+++ talks/qconsf2013/js/profiling.js
@@ -0,0 +1,53 @@
+(function() {
+gState = {
+  iteration: 0,
+  time: 0,
+  timeString: null,
+  buffer: null
+}
+
+function loop(num) {
+  console.profile("loop");
+  var i = 0;
+  function step(j) {
+    slowCall(i);
+    updateState(i);
+    console.log(i+" "+Date.now())
+    if (i++ < num) {
+      window.setTimeout(step, 10);
+    } else {
+      console.profileEnd("loop");
+      console.log("Finished");
+    }
+  }
+  step();
+}
+
+function updateState(i) {
+  gState.iteration = i;
+  gState.time = Date.now();
+  gState.timeString = new Date(gState.time).toUTCString();
+}
+
+function slowCall() {
+  initialize();
+  work();
+}
+
+function initialize() {
+  gState.buffer = "blah ";
+}
+
+function work() {
+  for (var i = 0; i < 1000; i++) {
+    gState.buffer += eval("1+1");
+  }
+}
+
+window.addEventListener("load", function(event) {
+  var profilingBtn = document.querySelector("#profiling-btn");
+  profilingBtn.addEventListener("click", function onClick(event) {
+    loop(500);
+  }, false);
+}, false);
+})();
