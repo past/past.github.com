ee9725db0168a5c41a5347cef2087999b5727d0f
Panagiotis Astithas <pastith@gmail.com>
2013-11-26 20:24:42 +0200

    AppDays 2013 talk

---
+++ talks/appdays2013/js/frameworks.js
@@ -0,0 +1,24 @@
+(function() {
+  window.addEventListener("load", function(event) {
+    var frameworkBtn = $("#framework-btn");
+    $("#framework-btn").on("click", function(event) {
+      run();
+    });
+  }, false);
+
+  function run() {
+    $("li").each(function(index) {
+      debugger;
+      createText(index, $(this).text()).then(function(text) {
+        console.log(text);
+        return index;
+      });
+    });
+  }
+
+  function createText(index, text) {
+    var result = $.Deferred();
+    result.resolve(index + ": " + text);
+    return result;
+  }
+})();
