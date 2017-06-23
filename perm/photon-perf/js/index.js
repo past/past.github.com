7d4ef43894597b865a260e47c746d05e4be80ee4
Panagiotis Astithas <pastith@gmail.com>
2017-06-23 17:21:32 +0300

    Add photon-perf dashboard

---
+++ perm/photon-perf/js/index.js
@@ -0,0 +1,53 @@
+var plots = [
+  {
+    "channel": "nightly",
+    "version": "",
+    "metric": "FX_NEW_WINDOW_MS",
+    "useSubmissionDate": false,
+    "sanitize": true,
+    "trim": true,
+    "compare": "",
+    "sensibleCompare": true,
+    "evoVersions": "3",
+    "filters": {
+      "application": "Firefox"
+    }
+  },
+  {
+    "channel": "nightly",
+    "version": "",
+    "metric": "FX_TAB_SWITCH_TOTAL_E10S_MS",
+    "useSubmissionDate": false,
+    "sanitize": true,
+    "trim": true,
+    "compare": "",
+    "sensibleCompare": true,
+    "evoVersions": "3",
+    "filters": {
+      "application": "Firefox"
+    }
+  },
+  {
+    "channel": "nightly",
+    "version": "",
+    "metric": "FX_TAB_CLOSE_TIME_ANIM_MS",
+    "useSubmissionDate": false,
+    "sanitize": true,
+    "trim": true,
+    "compare": "",
+    "sensibleCompare": true,
+    "evoVersions": "3",
+    "filters": {
+      "application": "Firefox"
+    }
+  }
+];
+
+var nodes = ["winOpen", "tabSwitch", "tabClose"];
+
+window.addEventListener('load', () => {
+  let i = 0;
+  for (plot of plots) {
+    TelemetryWrapper.go(plot, document.getElementById(nodes[i++]));
+  }
+});
